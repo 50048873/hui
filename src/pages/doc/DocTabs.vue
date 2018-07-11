c20078e2a856b909a221619606abb0b6072b0ec5
50048873 <50048873@qq.com>
2018-07-11 21:47:14 +0800

    try resolve sub-component double-direction data problem, but not success

--- src/pages/doc/DocTabs.vue
+++ src/pages/doc/DocTabs.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <p>tabs文档开发中...</p>
+    <input type="text" v-model="testTabs" style="border:1px solid #ddd;">
     <hui-tabs :current.sync="current">
       <hui-tab label="标签1" index="1">
         <span slot="label">查抄标签</span>
         <div>内容1</div>
       </hui-tab>
       <hui-tab label="标签2" index="2">
-        <div>内容2</div>
+        <div>内容2{{testTabs}}</div>
       </hui-tab>
     </hui-tabs>
   </div>
@@ -17,13 +18,14 @@
 export default {
   data () {
     return {
-      current: '1'
+      current: '2',
+      testTabs: ''
     }
   },
   mounted () {
-    setTimeout(() => {
-      this.current = '2'
-    }, 2000)
+    // setTimeout(() => {
+    //   this.current = '2'
+    // }, 2000)
   }
 }
 </script>
